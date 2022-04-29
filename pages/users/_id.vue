0ee05725d93e3bba4986f35ebc6eca6971f3abb7
father <myceliumv@gmail.com>
2022-04-29 22:53:02 +0300

    ADD nuxtlink

---
+++ pages/users/_id.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Пользователь {{ id }}</h1>
+</template>
+
+<script>
+export default {
+    
+    computed: {
+        id () {
+            return this.$route.params.id
+        }
+    }
+}
+</script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,3 +1,9 @@
 <template>
+    <div>
     <h1>Пользователи</h1>
+    <nuxt-link to="/users/1">
+    Перейти к Зему пользователя
+    </nuxt-link>
+    
+    </div>
 </template>
